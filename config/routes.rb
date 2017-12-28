2275bb6813044a31a8c4556af633c0f39f00fc6b
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-12-28 19:45:12 +0000

    changes to assignment on afternoon of 12/28

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/edit'
-
+  resources :advertisements
   resources :posts
 
   get 'about' => 'welcome#about'
+  root 'welcome#index'
   
 end
