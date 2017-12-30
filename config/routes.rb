4b84916668c3095b52729d0299efe3ee0b65659e
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-12-30 02:08:00 +0000

    first commits for checkpoint 19 assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'questions/title:'
+
+  get 'questions/string,'
+
+  get 'questions/body:'
+
+  get 'questions/text,'
+
+  get 'questions/resolved:'
+
+  get 'questions/boolean'
+
   resources :posts
 
   get 'about' => 'welcome#about'
