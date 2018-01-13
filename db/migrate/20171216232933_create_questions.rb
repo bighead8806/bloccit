97da25ce037cc74d2e9bf6eae715ab2ffbac8e62
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2018-01-13 03:10:00 +0000

    finally added resolved

--- db/migrate/20171216232933_create_questions.rb
+++ db/migrate/20171216232933_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
     create_table :questions do |t|
       t.string :title
       t.text :body
+      t.boolean :resolved
 
       t.timestamps
     end
