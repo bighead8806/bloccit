d7e1765d92eb8426f726bd11d953b650d2582bad
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2018-01-19 14:19:37 +0000

    ads seedsed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,15 @@ require 'random_data'
    )
  end
  
+ 50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: RandomData.random_integer
+  )
+end
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Advertisement.count} advertisements created"
