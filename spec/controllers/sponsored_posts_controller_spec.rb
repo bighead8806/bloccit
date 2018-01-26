15bdbec91a57d741b4cf9b781786eafd14ef712f
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2018-01-26 14:05:18 +0000

    added random_price

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -3,7 +3,8 @@ include RandomData
 
 RSpec.describe SponsoredPostsController, type: :controller do
     let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
-    let(:sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+    let(:sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData
+    .random_price)}
     
     describe "GET show" do
      it "returns http success" do
@@ -19,6 +20,42 @@ RSpec.describe SponsoredPostsController, type: :controller do
      it "assigns sponsored_post to @sponsored_post" do
        get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
        expect(assigns(:sponsored_post)).to eq(sponsored_post)
+     end
+   end
+   
+   describe "GET new" do
+      it "returns http success" do
+        get :new, params: { topic_id: my_topic.id, id: sponsored_post.id }
+        expect(response).to have_http_status(:success)
+      end
+      it "renders the #new view" do
+        get :new, params: { topic_id: my_topic.id, id: sponsored_post.id }
+        expect(response).to render_template :new
+      end
+      it "instantiates @sponsored_post" do
+        get :new, params: { topic_id: my_topic.id, id: sponsored_post.id}
+        expect(assigns(:sponsored_post)).not_to be_nil
+      end
+    end
+    
+    describe "GET edit" do
+     it "returns http success" do
+       get :edit, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "renders the #edit view" do
+       get :edit, topic_id: my_topic.id, id: sponsored_post.id
+       expect(response).to render_template :edit
+     end
+ 
+     it "assigns post to be updated to @sponsored_post" do
+       get :edit, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       sponsored_instance = assigns(:sponsored_post)
+ 
+       expect(sponsored_instance.id).to eq sponsored_post.id
+       expect(sponsored_instance.title).to eq sponsored_post.title
+       expect(sponsored_instance.body).to eq sponsored_post.body
      end
    end
 end
