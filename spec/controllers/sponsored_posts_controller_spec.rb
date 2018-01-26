2eb500728e7364f80e42691c431746ac854c3d33
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2018-01-26 06:46:09 +0000

    added GET show to spec

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe SponsoredPostsController, type: :controller do
-
+    let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
+    let(:sponsored_post) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+    
+    describe "GET show" do
+     it "returns http success" do
+       get :show, params: {topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to have_http_status(:success)
+     end
+     
+     it "renders the #show view" do
+       get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(response).to render_template(:show)
+     end
+ 
+     it "assigns sponsored_post to @sponsored_post" do
+       get :show, params: { topic_id: my_topic.id, id: sponsored_post.id }
+       expect(assigns(:sponsored_post)).to eq(sponsored_post)
+     end
+   end
 end
