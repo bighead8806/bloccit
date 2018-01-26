40c5f95ff67253f2c36f7f7d4b9be73dad058146
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2018-01-26 02:12:32 +0000

    removed index from post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,4 @@
 class PostsController < ApplicationController
-  #def index
-    #@posts = Post.all
-    #@posts.each_with_index do |post, index|
-     # if index % 5 == 0
-        #post.title = "SPAM"
-      #end 
-    #end 
-  #end
-
   def show
       @post = Post.find(params[:id])
   end
